1cc12f77eeeb266b7d83f453b74e23b1eefe328e
Tereza Smolová <framboiseart@email.cz>
2023-07-09 16:34:22 +0200

    Add Eleventy to Project

---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+module.exports = function(eleventyConfig) {
+    // Výchozí výstupní složka je: _site
+    // Zkopírovat images/ do _site/images
+    eleventyConfig.addPassthroughCopy("images");
+    // Zkopírovat css/ to _site/css/
+    eleventyConfig.addPassthroughCopy("css");
+
+    eleventyConfig.addPassthroughCopy("icons");
+
+    eleventyConfig.addPassthroughCopy("flavicon")
+
+    return {
+        // možné formáty šablon
+        templateFormats: ["njk", "html", "md"],
+        // jako šablonovací jazyk zvolíme Nunjucks`
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+      }    
+   };
