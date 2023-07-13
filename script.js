f38768e47037fe13a5777189da7a2649d596db8d
Tereza Smolova <tereza.smolova@gmail.com>
2023-07-14 00:26:10 +0200

    Filters

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 console.log('js funguje');
 
-
-// Získání odkazů na přepínače filtrů a seznam položek
 const filterToggles = document.querySelectorAll('.filter-toggle');
 const itemList = document.getElementById('item-list');
 
-// Přidání posluchačů událostí pro kliknutí na přepínače
 filterToggles.forEach((toggle) => {
-  toggle.addEventListener('click', filterItems);
+  toggle.addEventListener('click', toggleFilter);
 });
 
-// Funkce pro filtrování položek
+function toggleFilter() {
+  this.classList.toggle('active');
+  filterItems();
+}
+
 function filterItems() {
   const selectedTags = [];
 
-  // Zjištění vybraných tagů
   filterToggles.forEach((toggle) => {
     if (toggle.classList.contains('active')) {
       selectedTags.push(toggle.getAttribute('data-tag'));
     }
   });
 
-  // Procházení položek a upravování zobrazení
   const items = itemList.getElementsByTagName('li');
   for (let i = 0; i < items.length; i++) {
     const tags = items[i].getAttribute('data-tags');
+    const itemTags = tags.split(' ');
 
-    if (selectedTags.length === 0 || hasAnyTag(tags, selectedTags)) {
+    if (selectedTags.length === 0 || selectedTags.some(tag => itemTags.includes(tag))) {
       items[i].classList.remove('hide');
     } else {
       items[i].classList.add('hide');
@@ -34,15 +34,4 @@ function filterItems() {
   }
 }
 
-// Pomocná funkce pro kontrolu, zda prvek obsahuje alespoň jeden z vybraných tagů
-function hasAnyTag(tags, selectedTags) {
-  const itemTags = tags.split(' ');
-
-  for (let i = 0; i < selectedTags.length; i++) {
-    if (itemTags.includes(selectedTags[i])) {
-      return true;
-    }
-  }
-
-  return false;
-}
+filterItems();
