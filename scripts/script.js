437d034eda01dbf59ddfcd7da8cf263b7b21bee5
Tereza Smolova <tereza.smolova@gmail.com>
2023-07-18 09:44:39 +0200

    Repair

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,33 @@
-const filterToggles = document.querySelectorAll('.filter-toggle');
-const itemList = document.getElementById('item-list');
 
-filterToggles.forEach((toggle) => {
-  toggle.addEventListener('click', toggleFilter);
-});
 
-function toggleFilter() {
-  this.classList.toggle('active');
-  filterItems();
-}
-
-function filterItems() {
-  const selectedTags = [];
-
-  filterToggles.forEach((toggle) => {
-    if (toggle.classList.contains('active')) {
-      selectedTags.push(toggle.getAttribute('data-tag'));
+// Funkce pro filtrování položek
+function filterItems(tag) {
+  const itemList = document.getElementById('item-list');
+  const items = itemList.getElementsByClassName('links__item');
+  
+  // Skryje všechny položky
+  Array.from(items).forEach(function(item) {
+    item.style.display = 'none';
+  });
+  
+  // Zobrazí položky s odpovídajícím datovým značkou
+  Array.from(items).forEach(function(item) {
+    const tags = item.getAttribute('data-tags').split(' ');
+    if (tags.includes(tag)) {
+      item.style.display = 'block';
     }
   });
-
-  const items = itemList.getElementsByTagName('li');
-  for (let i = 0; i < items.length; i++) {
-    const tags = items[i].getAttribute('data-tags');
-    const itemTags = tags.split(' ');
-
-    if (selectedTags.length === 0 || selectedTags.some(tag => itemTags.includes(tag))) {
-      items[i].classList.remove('hide');
-    } else {
-      items[i].classList.add('hide');
-    }
-  }
+  
+  // Odscrollování na místo s vyfiltrovaným seznamem
+  const section = document.getElementById('item-list').parentElement;
+  section.scrollIntoView({ behavior: 'smooth' });
 }
 
-filterItems();
+// Přidání posluchačů událostí na tlačítka filtrů
+const filterButtons = document.getElementsByClassName('filter-toggle');
+Array.from(filterButtons).forEach(function(button) {
+  button.addEventListener('click', function() {
+    const tag = this.getAttribute('data-tag');
+    filterItems(tag);
+  });
+});
